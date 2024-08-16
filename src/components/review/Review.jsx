5af70e75764ec43a999c575874ac5061c37aae06
herry-jacker <herryjacker4@gmail.com>
2024-08-16 16:13:24 +0630

    fixed: conflict

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -28,11 +28,7 @@ const Review = () => {
             <div className='container'>
                 <div className="review-header">
                     <h5 className="section-head">
-<<<<<<< HEAD
-                        <span>Coffee</span>
-=======
                         <span>Brewed Awakening</span>
->>>>>>> 6a222ee (modified: some style and data)
                     </h5>
                 </div>
                 <div className="review-body row">
