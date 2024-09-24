32bd3d4e8a5862ce5a588ca48227a881d6805dda
Herry-Jacker <herryjacker4@gmail.com>
2024-09-24 22:56:39 +0630

    about section modifying

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,13 +8,9 @@ const About = () => {
         <section className='about' id='About'>
             <div className='container'>
                 <div className="contant">
-                    <h5>
-                        <span>About Us</span>
-                        <br/>
-                        We create
-                        <br/>
-                        branded coffee
-                    </h5>
+                    <h1 className='heading'>Subscribe to our newsletter for the<br/>
+                    latest update and offers.
+                    </h1>
                     <div className='divider'> </div>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, porro nesciunt at consequatur vitae accusantium repellendus blanditiis, quidem dolorum officiis modi et laudantium nemo ipsa omnis aspernatur repudiandae ut eius iure maiores maxime fugit inventore voluptatum. Cum beatae, delectus distinctio officia mollitia fugit molestias ipsam libero maxime impedit. Dolor reiciendis consectetur odio doloremque possimus.</p>
                 </div>
