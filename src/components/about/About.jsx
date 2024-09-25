59b418691af5c152fcc2469a8b6cdaf6dec5a91a
Herry-Jacker <herryjacker4@gmail.com>
2024-09-25 22:50:12 +0630

    modified: ui about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,11 +8,11 @@ const About = () => {
         <section className='about' id='About'>
             <div className='container'>
                 <div className="contant">
-                    <h1 className='heading'>Subscribe to our newsletter for the<br/>
-                    latest update and offers.
+                    <h1 className='heading'>Elvating Coffee with 35+<br/>
+                    Years Expertise
                     </h1>
-                    <div className='divider'> </div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, porro nesciunt at consequatur vitae accusantium repellendus blanditiis, quidem dolorum officiis modi et laudantium nemo ipsa omnis aspernatur repudiandae ut eius iure maiores maxime fugit inventore voluptatum. Cum beatae, delectus distinctio officia mollitia fugit molestias ipsam libero maxime impedit. Dolor reiciendis consectetur odio doloremque possimus.</p>
+                    <p>Java Time Caffee leads with over 35 years of expertise in coffee, setting the standard for excellence. Our advanced POS systems and toptier machinary ensure an expectional coffee experience. With focus on sharing knowlage and dedication to quality, we're more than just a coffee shop - we're innovators in ihe industry.</p>
+                    <p className='underline-btn'>Explore More</p>
                 </div>
                 <div className='image-slide-container'>
                     <SvgSlider src={ABOUT_iMGS}/>
