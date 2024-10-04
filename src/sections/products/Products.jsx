0e7ae532eafdb37147f2c448ad181820ded03528
Herry-Jacker <herryjacker4@gmail.com>
2024-10-04 22:28:25 +0630

    change folder structure for menu book and start create products section

---
+++ src/sections/products/Products.jsx
@@ -0,0 +1,16 @@
+import './products.css';
+
+
+const Products = () => {
+
+    return (
+        <section id="Products">
+            <div className="container">
+                <h1 className='heading-black'>Our Top Featured<br/>
+                Categories</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Products;
