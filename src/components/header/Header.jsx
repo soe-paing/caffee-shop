7343ff88998ba92ae72d0e6bad3045b337c3b632
Soe Paing <herryjacker4@gmail.com>
2024-10-17 13:44:23 +0630

    fix header overflow bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,42 +20,43 @@ const Header = () => {
     return (
         <header
             ref={headerRef}
-            className="header"
             onScroll={() => setIsActive(false)}>
-            <a href="#" className="logo">
-                <img src={logo} alt="javatime logo" />
-            </a>
-            <nav className={`${isActive} navbar`}>
-                <CloseRight setFalse={() => setIsActive(false)}/>
-                { NAV_DATA.map( (nav, index) => <a key={index} href={nav}>{nav}</a>) }
-            </nav>
-            <div className='icons'>
-                <div 
-                    className="fas fa-bag-shopping" 
-                    id='cart-btn'
-                    onClick={() => {
-                        isActive == 'active-cart' ?
-                        setIsActive(false) : setIsActive('active-cart');
-                    }}></div>
-                <div 
-                    className="fas fa-user" 
-                    id='cart-btn'
-                    onClick={() => {
-                        isActive == 'active-cart' ?
-                        setIsActive(false) : setIsActive('active-cart');
-                    }}></div>
-                <div 
-                    className="fas fa-bars" 
-                    id='menu-btn'
-                    onClick={() => {
-                        isActive == 'active-nav' ?
-                        setIsActive(false) : setIsActive('active-nav');
-                    }}></div>
-            </div>
-            <div className={`${isActive} cart-items-container`}>
-                <CloseRight setFalse={() => setIsActive(false)}/>
-                { CART_ITEMS.map( (item, index) => <CartItem key={index} {...item}/>) }
-                <Btn1 rounded={true}>Checkout</Btn1>
+            <div className="header">
+                <a href="#" className="logo">
+                    <img src={logo} alt="javatime logo" />
+                </a>
+                <nav className={`${isActive} navbar`}>
+                    <CloseRight setFalse={() => setIsActive(false)}/>
+                    { NAV_DATA.map( (nav, index) => <a key={index} href={nav}>{nav}</a>) }
+                </nav>
+                <div className='icons'>
+                    <div 
+                        className="fas fa-bag-shopping" 
+                        id='cart-btn'
+                        onClick={() => {
+                            isActive == 'active-cart' ?
+                            setIsActive(false) : setIsActive('active-cart');
+                        }}></div>
+                    <div 
+                        className="fas fa-user" 
+                        id='cart-btn'
+                        onClick={() => {
+                            isActive == 'active-cart' ?
+                            setIsActive(false) : setIsActive('active-cart');
+                        }}></div>
+                    <div 
+                        className="fas fa-bars" 
+                        id='menu-btn'
+                        onClick={() => {
+                            isActive == 'active-nav' ?
+                            setIsActive(false) : setIsActive('active-nav');
+                        }}></div>
+                </div>
+                <div className={`${isActive} cart-items-container`}>
+                    <CloseRight setFalse={() => setIsActive(false)}/>
+                    { CART_ITEMS.map( (item, index) => <CartItem key={index} {...item}/>) }
+                    <Btn1 rounded={true}>Checkout</Btn1>
+                </div>
             </div>
         </header>
     )
