5f37cd87433d185f914810858c9f294c09e56e71
Herry-Jacker <herryjacker4@gmail.com>
2024-12-04 20:44:13 +0630

    changed cart with modal dialog instead of cart slide

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,31 @@
 import './header.css'
-import CartItem from '../cartItem/CartItem';
-import { CART_ITEMS, NAV_DATA} from '../../data';
-import Btn1, { CloseRight } from '../buttons/Btn1';
+import { NAV_DATA} from '../../data';
+import { CloseRight } from '../buttons/Btn1';
 import { useEffect, useRef, useState } from 'react';
 import logo from '../../assets/logo.jpg';
+import CartModal from '../cartModal/cartModal';
 
 const Header = () => {
     const [ isActive, setIsActive] = useState(false);
     const headerRef = useRef(null);
+    const modal = useRef();
+
+    function handleOpenCartClick() {
+      modal.current.open();
+      isActive == 'active-cart' ?
+      setIsActive(false) : setIsActive('active-cart');
+    }
+    
+    let modalActions = <button>Close</button>;
+
+    if (2 > 0) {
+      modalActions = (
+        <>
+          <button>Close</button>
+          <button>Checkout</button>
+        </>
+      );
+    }
 
     useEffect(() => {
         const header = headerRef.current;
@@ -18,47 +36,47 @@ const Header = () => {
     }, []);
     
     return (
-        <header
-            ref={headerRef}
-            onScroll={() => setIsActive(false)}>
-            <div className="header">
-                <a href="#" className="logo">
-                    <img src={logo} alt="javatime logo" />
-                </a>
-                <nav className={`${isActive} navbar`}>
-                    <CloseRight setFalse={() => setIsActive(false)}/>
-                    { NAV_DATA.map( (nav, index) => <a key={index} href={nav}>{nav}</a>) }
-                </nav>
-                <div className='icons'>
-                    <div 
-                        className="fas fa-bag-shopping" 
-                        id='cart-btn'
-                        onClick={() => {
-                            isActive == 'active-cart' ?
-                            setIsActive(false) : setIsActive('active-cart');
-                        }}></div>
-                    <div 
-                        className="fas fa-user" 
-                        id='cart-btn'
-                        onClick={() => {
-                            isActive == 'active-cart' ?
-                            setIsActive(false) : setIsActive('active-cart');
-                        }}></div>
-                    <div 
-                        className="fas fa-bars" 
-                        id='menu-btn'
-                        onClick={() => {
-                            isActive == 'active-nav' ?
-                            setIsActive(false) : setIsActive('active-nav');
-                        }}></div>
+        <>
+            <CartModal
+            ref={modal}
+            title="Your Cart"
+            actions={modalActions}
+            />
+            <header
+                ref={headerRef}
+                onScroll={() => setIsActive(false)}>
+                <div className="header">
+                    <a href="#" className="logo">
+                        <img src={logo} alt="javatime logo" />
+                    </a>
+                    <nav className={`${isActive} navbar`}>
+                        <CloseRight setFalse={() => setIsActive(false)}/>
+                        { NAV_DATA.map( (nav, index) => <a key={index} href={nav}>{nav}</a>) }
+                    </nav>
+                    <div className='icons'>
+                        <div 
+                            className="fas fa-bag-shopping" 
+                            id='cart-btn'
+                            onClick={handleOpenCartClick}
+                        ></div>
+                        <div 
+                            className="fas fa-user" 
+                            id='cart-btn'
+                            onClick={() => {
+                                isActive == 'active-cart' ?
+                                setIsActive(false) : setIsActive('active-cart');
+                            }}></div>
+                        <div 
+                            className="fas fa-bars" 
+                            id='menu-btn'
+                            onClick={() => {
+                                isActive == 'active-nav' ?
+                                setIsActive(false) : setIsActive('active-nav');
+                            }}></div>
+                    </div>
                 </div>
-                <div className={`${isActive} cart-items-container`}>
-                    <CloseRight setFalse={() => setIsActive(false)}/>
-                    { CART_ITEMS.map( (item, index) => <CartItem key={index} {...item}/>) }
-                    <Btn1 rounded={true}>Checkout</Btn1>
-                </div>
-            </div>
-        </header>
+            </header>
+        </>
     )
 }
 
